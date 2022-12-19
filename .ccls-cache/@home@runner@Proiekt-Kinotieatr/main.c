4896f1e60a064f1443ef26d3d7219761dbdd7213
Ксения <120342870+KseniaShaveko@users.noreply.github.com>
2022-12-19 05:56:00 +0000

    Изменена ф-я добавления фильма

--- .ccls-cache/@home@runner@Proiekt-Kinotieatr/main.c
+++ .ccls-cache/@home@runner@Proiekt-Kinotieatr/main.c
@@ -6,13 +6,13 @@
 //#include <string.h>     в         будующем
 
 typedef struct Film {      //создание структуры фильма
-  int index;
-  char name[80];         // название фильма
-  char year[8];          // год выпуска
-  char country[40];      //  название страны/стран производителя
-  char genre[45];        //  название жанра
-  char rating[5];         // рейтинг 
-} film;                   // переименование
+  int index;     
+  char name[100];           // название фильма
+  char year[6];            // год выпуска
+  char country[40];        //  название страны/стран производителя
+  char genre[50];          //  название жанра
+  char rating[5];          // рейтинг 
+} film;                    // переименование
 
 typedef struct Knot { // структура узел (двусвязный список фильмов)
   film a;             // какой-то фильм со всеми его данными
@@ -38,8 +38,8 @@ typedef struct User {    // структура пользователя
 
 
 
-knot * init_film(film a){              //инициализируем первый элемент списка
-  knot *list;                          //создали узел
+knot* init_film(film a){              //инициализируем первый элемент списка
+  knot* list;                          //создали узел
   list = (knot*)malloc(sizeof(knot));  // выделели память
   list -> a = a;                       // узел, хранящий инфу о фильме
   list -> prev = list;                 // предыдущий указывает на себя
@@ -47,36 +47,29 @@ knot * init_film(film a){              //инициализируем первы
   return list;                         // вернули
 }
 
-void add(knot* list, film a){          //функция добавления фильма
-  knot* temp;
-  knot* temp_next;
-  knot* temp_prev;
-  temp = (knot*)malloc(sizeof(knot));
-  temp_next = list -> next;
-  temp_prev = list -> prev;
-  if (temp_prev ==NULL){ 
-    list -> prev = list;
+void add(knot* list, film a) {  // ф-я добавления фильма в список
+  knot* temp = list;
+  while (temp->next != list) {//пока следующий не равен первому - бежим к последнему
+    temp = temp->next;
   }
-  if (temp_prev == list){
-    temp_prev = list -> next;
-  }
-  list -> next = temp;
-  temp -> a = a;
-  temp -> next = temp_next;
-  temp -> prev = list;
-  temp_next -> prev = temp;
+  temp->next = (knot*)malloc(sizeof(knot));
+  temp->next->prev = temp;
+  temp = temp->next;
+  temp->next = list;
+  list->prev = temp;
+  temp->a = a;
 }
 
 void print(knot* list){  // функция принта
   knot* w;
   w = list;
-  do{
+  do {
     printf("%s\n", w -> a.name);
     printf("%s\n", w -> a.year);
     printf("%s\n", w -> a.country);
     printf("%s\n", w -> a.genre);
     printf("%s\n\n", w -> a.rating);
-    w = w -> prev;
+    w = w -> next;
   } while (w != list);
 }
 
@@ -88,20 +81,20 @@ int main(void) {
   knot* list;
   int index = 1;
   a.index = index;
-  fgets(a.name, 80, open_film);
-  fgets(a.year, 8, open_film);  
+  fgets(a.name, 100, open_film);
+  fgets(a.year, 6, open_film);  
   fgets(a.country, 40, open_film);  
-  fgets(a.genre, 45, open_film);  
+  fgets(a.genre, 50, open_film);  
   fgets(a.rating, 5, open_film);
   list = init_film(a);
   while (!feof(open_film)){
     film a;
     ++index;
     a.index = index;
-    fgets(a.name, 80, open_film);
-    fgets(a.year, 8, open_film);  
+    fgets(a.name, 100, open_film);
+    fgets(a.year, 6, open_film);  
     fgets(a.country, 40, open_film);  
-    fgets(a.genre, 45, open_film);  
+    fgets(a.genre, 50, open_film);  
     fgets(a.rating, 5, open_film);
     add(list, a);
   }
